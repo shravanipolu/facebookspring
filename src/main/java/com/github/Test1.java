61c6237a54834d92ac4e5f7c7d033a99181502cc
Shravani <Shravani@Shravani>
2019-06-11 17:31:41 +0530

    stashing

--- src/main/java/com/github/Test1.java
+++ src/main/java/com/github/Test1.java
@@ -3,7 +3,7 @@ package com.github;
 public class Test1 {
 public static void main(String[] args) {
 	System.out.println("hai git");
-
+System.out.println("java");
 	System.out.println("hello from branch2");
 
 	System.out.println("hello from branch1");
