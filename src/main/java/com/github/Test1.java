c0d51f133283308e0f9f632e342ed6c894a59047
Shravani <Shravani@Shravani>
2019-06-11 17:34:25 +0530

    hai

--- src/main/java/com/github/Test1.java
+++ src/main/java/com/github/Test1.java
@@ -2,8 +2,12 @@ package com.github;
 
 public class Test1 {
 public static void main(String[] args) {
+
 	System.out.println("hai git");
 System.out.println("java");
+	System.out.println("hai git hub12");
+
+
 	System.out.println("hello from branch2");
 
 	System.out.println("hello from branch1");
