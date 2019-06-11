297ce7b1ee1ba38288f2bc02623764d983ccde5c
Shravani <Shravani@Shravani>
2019-06-11 17:27:01 +0530

    commit

--- src/main/java/com/github/Test1.java
+++ src/main/java/com/github/Test1.java
@@ -2,10 +2,11 @@ package com.github;
 
 public class Test1 {
 public static void main(String[] args) {
-	System.out.println("hai git");
+	System.out.println("hai github@123");
 
 	System.out.println("hello from branch2");
 	System.out.println("hello from branch4");
 	System.out.println("hello from branch1");
+
 }
 }
