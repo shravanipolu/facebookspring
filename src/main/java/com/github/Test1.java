d15064ef86b4cb481d863e8a3857ba87e805485a
Shravani <Shravani@Shravani>
2019-06-11 17:38:26 +0530

    conflict

--- src/main/java/com/github/Test1.java
+++ src/main/java/com/github/Test1.java
@@ -5,7 +5,7 @@ public static void main(String[] args) {
 
 	System.out.println("hai git");
 System.out.println("java");
-	System.out.println("hai git hub12");
+	System.out.println("hai git hub 12");
 
 
 	System.out.println("hello from branch2");
