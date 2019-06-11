c3251107e46799eb2e205627ce3ffedacdf3748a
Shravani <Shravani@Shravani>
2019-06-11 17:09:42 +0530

    changes added

--- src/main/java/com/github/Test1.java
+++ src/main/java/com/github/Test1.java
@@ -5,7 +5,7 @@ public static void main(String[] args) {
 	System.out.println("hai git");
 
 	System.out.println("hello from branch2");
-
+	System.out.println("hello from branch4");
 	System.out.println("hello from branch1");
 }
 }
